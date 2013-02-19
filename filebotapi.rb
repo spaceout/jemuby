eebb72dc0fe4ed8e3fa0752e562593cd7549af4c
jemily <jemily@monitor>
2013-02-18 20:16:40 -0500

    Working v0.5 beta

---
+++ filebotapi.rb
@@ -0,0 +1,31 @@
+require 'logger'
+require 'xmlsimple'
+
+class FileBotAPI
+  def initialize(logger = nil)
+    @log = logger || Logger.new(STDOUT)
+  end
+
+  def filebot_rename(base_path, tvshow_basepath, filebot_log)
+    @log.info "Begin FileBot Rename"
+    `filebot -rename #{base_path} --db thetvdb --format \"#{tvshow_basepath}/{n}/{n} - s{s.pad(2)}e{es*.pad(2)join('e')} - {t}\" -non-strict`
+    if $? == 0
+      @log.info "Filebot Rename Successful!"
+      get_history(filebot_log)
+    elsif $? != 0
+      @log.error "ERROR WITH RENAME"
+      abort("FATAL ERROR WITH RENAME")
+    end
+  end
+
+  def get_history(filebot_log)
+    filebot_history = XmlSimple.xml_in(filebot_log)
+    @log.info "FileBot History From: #{filebot_history['sequence'].last['date']}"
+    filebot_history['sequence'].last['rename'].each do |rename_item|
+      @log.info "FROM: " + rename_item['from'] + "TO: " + rename_item['to']
+    end
+    @log.info "End FileBot History"
+  end
+
+end
+
--- test-jemuby.rb
+++ test-jemuby.rb
@@ -3,40 +3,42 @@ require "fileutils"
 require "logger"
 require_relative 'filemanipulator'
 require_relative 'xmissionapi'
+require_relative 'xbmcapi'
+require_relative 'filebotapi'
 
-#BASE_PATH = '/home/jemily/renametest/test'
-#TVSHOW_BASEPATH = "/home/jemily/renametest/test2"
-BASE_PATH = '/home/jemily/finished/tvshows'
-TVSHOW_BASEPATH = '/mnt/tvshows'
+BASE_PATH = '/home/jemily/renametest/test'
+TVSHOW_BASEPATH = "/home/jemily/renametest/test2"
+#BASE_PATH = '/home/jemily/finished/tvshows'
+#TVSHOW_BASEPATH = '/mnt/tvshows'
+#MIN_VIDEOSIZE = 50000000
 XBMC_HOSTNAME = "nuggetron"
 XBMC_PORT = "9090"
-MIN_VIDEOSIZE = 50000000
+MIN_VIDEOSIZE = 50
 TRANSMISSION_URL = "http://192.168.1.8:9091/transmission/rpc"
 TRANSMISSION_USER = "transmission"
 TRANSMISSION_PASSWORD = "transmission"
+FILEBOT_LOG = '/home/jemily/.filebot/history.xml'
 
-def setup_logger()
-  @log = Logger.new(STDOUT)
-  @log.level = Logger::INFO
-  @log.formatter = proc do |severity, datetime, progname, msg|
-    "[#{datetime}] #{severity}: #{msg}\n"
-  end
-  @log.info "Logger Initialized"
+log = Logger.new(STDOUT)
+log.level = Logger::INFO
+log.formatter = proc do |severity, datetime, progname, msg|
+  "[#{datetime}] #{severity}: #{msg}\n"
 end
+log.info "Logger Initialized"
 
-setup_logger()
-fm = FileManipulator.new(@log)
-pp = PostProcessor.new(@log)
-
-xmission = XmissionApi.new(
-  :username => TRANSMISSION_USER,
-  :password => TRANSMISSION_PASSWORD,
-  :url      => TRANSMISSION_URL
-)
-
+processGo = false
+log.info "Transmission processing Started"
+xmission = XmissionApi.new(:username => TRANSMISSION_USER,:password => TRANSMISSION_PASSWORD,:url => TRANSMISSION_URL)
 xmission.all.each do |download|
-    puts download["id"] if download["isFinished"] == true && download["downloadDir"] == BASE_PATH
+  if download["isFinished"] == true && download["downloadDir"] == BASE_PATH
+    log.warn "Removing #{download["name"]} from Transmission"
+    xmission.remove(download["id"])
+    processGo = true
+  end
 end
+log.info "Transmission processing Complete"
+
+fm = FileManipulator.new(log)
 
 Dir.chdir(BASE_PATH)
 Dir.glob("*").each do |dir_entry|
@@ -47,7 +49,10 @@ Dir.glob("*").each do |dir_entry|
   end
 end
 
-pp.filebot_rename(BASE_PATH, TVSHOW_BASEPATH)
-pp.update_xbmc(XBMC_HOSTNAME, XBMC_PORT)
+fb = FileBotAPI.new(log)
+fb.filebot_rename(BASE_PATH, TVSHOW_BASEPATH, FILEBOT_LOG)
 
-@log.info "Script Completed successfully"
+xb = XbmcApi.new(log)
+#xb.update_xbmc(XBMC_HOSTNAME, XBMC_PORT)
+
+log.info "Script Completed successfully"
