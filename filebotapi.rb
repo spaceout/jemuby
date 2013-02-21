9e038056d6098e201cb38cea37ec2f492a3af5e9
jemily <jemily@monitor>
2013-02-20 19:31:49 -0500

    working 1.0RC2

--- filebotapi.rb
+++ filebotapi.rb
@@ -8,12 +8,13 @@ class FileBotAPI
 
   def filebot_rename(base_path, tvshow_basepath, filebot_log)
     @log.info "Begin FileBot Rename"
-    `filebot -rename #{base_path} --db thetvdb --format \"#{tvshow_basepath}/{n}/{n} - s{s.pad(2)}e{es*.pad(2)join('e')} - {t}\" -non-strict`
+    fb_result = `filebot -rename #{base_path} --db thetvdb --format \"#{tvshow_basepath}/{n}/{n} - s{s.pad(2)}e{es*.pad(2)join('e')} - {t}\" -non-strict 2>&1`
     if $? == 0
       @log.info "Filebot Rename Successful!"
       get_history(filebot_log)
     elsif $? != 0
       @log.error "ERROR WITH RENAME"
+      puts fb_result
       abort("FATAL ERROR WITH RENAME")
     end
   end
